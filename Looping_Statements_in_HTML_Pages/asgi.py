872fc46bcb0efd986426dc1c21c5d8c835b87641
karthik1256 <lkarthi1256@gmail.com>
2023-11-20 21:09:25 +0530

    first commit

---
+++ Looping_Statements_in_HTML_Pages/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Looping_Statements_in_HTML_Pages project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Looping_Statements_in_HTML_Pages.settings')
+
+application = get_asgi_application()
---
+++ apple/urls.py
@@ -0,0 +1,6 @@
+from apple.views import *
+from django.urls import path
+
+urlpatterns=[
+    path('forloop/',forloop,name='forloop'),
+]
